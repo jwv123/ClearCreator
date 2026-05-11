a18ae573ec4118e18fe937d972d364a94b6e46a3
jwv123 <79159979+jwv123@users.noreply.github.com>
2026-05-11 15:18:20 +0200

    Update environment.ts

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://vpfdiqcswdbjepmvgjuy.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwZmRpcWNzd2RiamVwbXZnanV5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODQzMjYzOSwiZXhwIjoyMDk0MDA4NjM5fQ.vmCQZ4fTC2mzlF9jvSOaS_UU4fTwanXM92NfE6kuDVs',
+  supabaseUrl: '',
+  supabaseAnonKey: '',
   graphqlUrl: 'http://localhost:3001/api/graphql',
   aiApiUrl: 'http://localhost:3001/api/ai',
 };
